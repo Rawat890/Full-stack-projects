dec47cef59ad0818b1765b569712e4bd50023f84
Rawat890 <rohanrawat7508@gmail.com>
2026-03-20 10:46:06 +0530

    chore: project deleted

--- gms_fitness_application/src/utilities/helper/getDimensions.tsx
+++
@@ -1,7 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const getDimensions = () => {
-  const screenWidth = Dimensions.get('window').width;
-  const screenHeight = Dimensions.get('window').height;
-  return [screenWidth, screenHeight];
-};
