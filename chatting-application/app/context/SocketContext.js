308175285ef97598c5da539e89a30001f448cc26
Rawat890 <rohanrawat7508@gmail.com>
2026-03-22 10:12:31 +0530

    chore: add SocketContext for managing WebSocket connections

---
+++ chatting-application/app/context/SocketContext.js
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { AuthContext } from "./AuthContext";
+
+
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+  return useContext(SocketContext)
+}
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [authUser, userId] = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localHost:6000", {
+        query: {
+          userId: userId
+        }
+      })
+      setSocket(socket)
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null)
+      }
+    }
+  }, [])
+
+  return (
+    <SocketContext.Provider value={{ socket, setSocket }}>
+      {children}
+    </SocketContext.Provider>
+  )
+
+}
