7da015b166a6ab49e0d99a8aaea4338c712fa1e4
Rawat890 <rohanrawat7508@gmail.com>
2026-03-24 16:01:58 +0530

    chore: AddNotes screen created

---
+++ Notes-app/screens/AddNotes.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function AddNotes() {
+  return (
+    <View>
+      <Text>AddNotes</Text>
+    </View>
+  )
+}
