d7c454c6bd2a73fddd951582084a36f65e3d3b78
Rawat890 <rohanrawat7508@gmail.com>
2026-03-25 16:04:56 +0530

    chore: createNote and Deletenote query added

--- Notes-app/app/queries/notesQuery.ts
+++ Notes-app/app/queries/notesQuery.ts
@@ -1,10 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAllNotes } from "../backend/notes";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createNotes, deleteNotes, getAllNotes } from "../backend/notes";
 
 //Creating a custom hook
 export const useNoteList = () => {
   return useQuery({
     queryKey: ["notes"], //caching the data (very important)
     queryFn: getAllNotes,
+  });
+};
+
+// add notes - mutation i.e. add, delete, update
+export const useCreateNote = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createNotes,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+};
+
+// delete notes
+export const useDeleteNote = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteNotes,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
   });
 };
