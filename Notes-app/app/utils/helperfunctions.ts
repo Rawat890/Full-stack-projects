21e314681a0c3cdae9b464a067b2c8af98d5e056
Rawat890 <rohanrawat7508@gmail.com>
2026-03-25 15:03:47 +0530

    chore: timeAgo function created to get past time

---
+++ Notes-app/app/utils/helperfunctions.ts
@@ -0,0 +1,22 @@
+export const timeAgo = (dateString: string) => {
+  const now = new Date();
+  const past = new Date(dateString);
+
+  const diffInSeconds = Math.floor((now.getTime() - past.getTime()) / 1000);
+
+  if (diffInSeconds < 60) return "Just now";
+
+  const minutes = Math.floor(diffInSeconds / 60);
+  if (minutes < 60) return `${minutes} minutes ago`;
+
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) return `${hours} hours ago`;
+
+  const days = Math.floor(hours / 24);
+  if (days < 7) return `${days} day${days > 1 ? "s" : ""} ago`;
+
+  return past.toLocaleDateString("en-IN", {
+    day: "numeric",
+    month: "short",
+  });
+};
