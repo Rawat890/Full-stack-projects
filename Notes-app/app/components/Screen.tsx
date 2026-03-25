ec5271df26eecc33123ba022c39caced7472566c
Rawat890 <rohanrawat7508@gmail.com>
2026-03-25 12:21:15 +0530

    chore: typescript interface applied

--- Notes-app/app/components/Screen.tsx
+++ Notes-app/app/components/Screen.tsx
@@ -1,8 +1,15 @@
+import { ReactNode } from "react";
+import { StyleProp, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Screen({ children }) {
+type ScreenProps = {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+};
+
+export default function Screen({ children, style }: ScreenProps) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={[{ flex: 1 }, style]}>
       {children}
     </SafeAreaView>
   );
