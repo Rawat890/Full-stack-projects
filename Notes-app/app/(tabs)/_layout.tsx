da71bd45ff305dfa2e61b8fb2bf9605e46d7151d
Rawat890 <rohanrawat7508@gmail.com>
2026-03-25 10:51:57 +0530

    chore: TabLayout for the application created and notes and profile tabs added

---
+++ Notes-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Notes',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="document-text" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
