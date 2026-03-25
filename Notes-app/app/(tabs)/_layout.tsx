2c6a230b3573cf0fb1a5b97d6bab2c5786d7e8b7
Rawat890 <rohanrawat7508@gmail.com>
2026-03-25 17:15:51 +0530

    chore: tab bar style updated

--- Notes-app/app/(tabs)/_layout.tsx
+++ Notes-app/app/(tabs)/_layout.tsx
@@ -1,15 +1,23 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import COLORS from '../utils/colors';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{headerShown: false}}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: COLORS.black },
+        tabBarActiveTintColor: COLORS.white,
+        tabBarInactiveTintColor: COLORS.gray,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Notes',
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="document-text" size={32} color={color} />
+            <Ionicons name="document-text" size={34} color={color} />
           ),
         }}
       />
@@ -18,7 +26,7 @@ export default function TabLayout() {
         options={{
           title: 'Profile',
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={32} color={color} />
+            <Ionicons name="person" size={34} color={color} />
           ),
         }}
       />
