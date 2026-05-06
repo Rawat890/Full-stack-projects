902024b788f7f0382c1e9d36225d32e579e52cea
Rawat890 <rohanrawat7508@gmail.com>
2026-05-06 16:37:34 +0530

    chore: root navigator for app created

---
+++ Bike-riding-app/src/navigation/RootNavigator.tsx
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { navigationRef } from "../utils/navigationService";
+import AppNavigator from "./AppNavigator";
+import AuthNavigator from "./AuthNavigator";
+
+
+const RootNavigator = () => {
+
+    const isAuthenticated = true;
+
+    return (
+        <NavigationContainer ref={navigationRef}>
+            {
+                isAuthenticated ? <AppNavigator /> : <AuthNavigator />
+            }
+        </NavigationContainer>
+    )
+}
