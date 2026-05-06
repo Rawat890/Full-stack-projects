f5a7704a869a388bd5a6fb980480cee1ee35a062
Rawat890 <rohanrawat7508@gmail.com>
2026-05-06 16:34:03 +0530

    chore: routes constant created

---
+++ Bike-riding-app/src/utils/routes.ts
@@ -0,0 +1,10 @@
+export const SCREENS = {
+    root: '/',
+    login : "Login",
+    Register: "Register",
+    Home: "Home",
+    RideDetails: "RideDetails",
+    Search: "Search",
+    SelectRide: "SelectRide",
+    RideTracker: "RideTracker"
+} as const;
