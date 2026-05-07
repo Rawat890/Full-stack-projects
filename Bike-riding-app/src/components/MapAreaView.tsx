fe68a78ffb059249e87e9200c5b99ad601c4bdfb
Rawat890 <rohanrawat7508@gmail.com>
2026-05-07 18:53:31 +0530

    chore: unnecessary view removed

--- Bike-riding-app/src/components/MapAreaView.tsx
+++ Bike-riding-app/src/components/MapAreaView.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 const MapAreaView = () => {
   return (
-    <View style={{ flex: 1 }}>
       <MapView
         initialRegion={{
           latitude: 12.0288,
@@ -16,7 +14,6 @@ const MapAreaView = () => {
       >
         <Marker coordinate={{ latitude: 12.0288, longitude: 77.59 }} />
       </MapView>
-    </View>
   )
 }
 
