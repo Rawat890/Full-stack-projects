c6ee0127bab6fb4c3e89e327a4d6c2b9a5df8494
Rawat890 <rohanrawat7508@gmail.com>
2026-05-07 18:55:31 +0530

    chore: dummy data added

---
+++ Bike-riding-app/src/utils/dummyData.ts
@@ -0,0 +1,78 @@
+export const recentPlaces = [
+  {
+    title: 'Home',
+    subtitle: '221B Baker Street, London',
+  },
+  {
+    title: 'Office',
+    subtitle: 'Cyber City, Gurugram',
+  },
+  {
+    title: 'Airport',
+    subtitle: 'Indira Gandhi International Airport, Delhi',
+  },
+  {
+    title: 'Coffee Shop',
+    subtitle: 'Sector 29 Market, Gurugram',
+  },
+  {
+    title: 'Gym',
+    subtitle: 'FitZone Fitness Club, Rohtak',
+  },
+  {
+    title: 'Mall',
+    subtitle: 'Ambience Mall, Gurugram',
+  },
+  {
+    title: 'Friend’s Place',
+    subtitle: 'Model Town, Panipat',
+  },
+  {
+    title: 'Restaurant',
+    subtitle: 'Connaught Place, New Delhi',
+  },
+];
+
+
+export const exploreOptions = [
+  {
+    title: 'Bike',
+    icon: '🏍️',
+  },
+  {
+    title: 'Auto',
+    icon: '🛺',
+  },
+  {
+    title: 'Car',
+    icon: '🚘',
+  },
+  {
+    title: 'Parcel',
+    icon: '📦',
+  },
+  {
+    title: 'Taxi',
+    icon: '🚕',
+  },
+  {
+    title: 'Premium',
+    icon: '✨',
+  },
+  {
+    title: 'Rental',
+    icon: '🚗',
+  },
+  {
+    title: 'Intercity',
+    icon: '🛣️',
+  },
+  {
+    title: 'Courier',
+    icon: '📬',
+  },
+  {
+    title: 'Delivery',
+    icon: '🛵',
+  },
+];
