53224cc60edb7c528a5f7373f044f48beb2ba9c7
Rawat890 <rohanrawat7508@gmail.com>
2026-05-10 18:20:37 +0530

    chore: isAuthenticated from the Auth context used to select navigator

--- Rapido-booking-app/src/navigation/RootNavigator.tsx
+++ Rapido-booking-app/src/navigation/RootNavigator.tsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useAuth } from "../context/AuthContext";
 import { navigationRef } from "../utils/navigationService";
 import AppNavigator from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 
 
 const RootNavigator = () => {
-
-    const isAuthenticated = false;
+    const {isAuthenticated} = useAuth();
+    console.log(isAuthenticated)
 
     return (
         <NavigationContainer ref={navigationRef}>
